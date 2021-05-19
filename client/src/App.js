ebd3176e7a60d0570e75bb40230340aff7d099ef
AlexDeLarge <jmrendon48@gmail.com>
2021-05-19 14:46:17 -0700

    initial file structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>THE WORLD IS A LONELY PLACE AND THE ONLY HOPE OF FINDING ONESELF IS THROUGH THE SACRAMENT KNOWN AS VIDEO GAMES.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
