901f203c467172d742cda5739b8fedaf0b49ab77
Joseph Prospero <jprospero413@gmail.com>
2021-05-19 16:34:45 -0700

    began homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <p>THE WORLD IS A LONELY PLACE AND THE ONLY HOPE OF FINDING ONESELF IS THROUGH THE SACRAMENT KNOWN AS VIDEO GAMES.</p>
-      </header>
+
+      <Home></Home>
+      
     </div>
   );
 }
