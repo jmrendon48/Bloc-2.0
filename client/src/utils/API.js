c587b8f48267f0dfce9308c329bfd967bc286c68
James Canlas <jamescanlasjr@yahoo.com>
2021-05-20 22:07:11 -0700

    typo

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -19,3 +19,21 @@ export const searchGame = (query) => {
         return data.json()
     })
 };
+
+export const searchId = (query) => {
+    fetch(`https://api.igdb.com/v4/games`, {
+        method: "POST",
+        headers: {
+          "Client-ID": client,
+          "Authorization": auth,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( `
+        fields name, cover; 
+        limit 10;
+        search ${query}";
+        `)
+    }).then(function(data) {
+        return data.json()
+    })
+};
