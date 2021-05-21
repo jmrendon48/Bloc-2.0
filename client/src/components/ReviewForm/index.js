0fa5884030e16b0847e5ceb55a32a2132f38789e
AlexDeLarge <jmrendon48@gmail.com>
2021-05-20 19:56:56 -0700

    added title textarea to ReviewForm

--- client/src/components/ReviewForm/index.js
+++ client/src/components/ReviewForm/index.js
@@ -1,37 +1,58 @@
 import React, { useState } from "react";
 
 const ReviewForm = () => {
-  const [thoughtText, setText] = useState("");
-  const [characterCount, setCharacterCount] = useState(0);
+  const [titleText, setTitleText] = useState("");
+  const [titleCharacterCount, setTitleCharacterCount] = useState(0);
 
-  const handleChange = (event) => {
+  const [bodyText, setBodyText] = useState("");
+  const [bodyCharacterCount, setBodyCharacterCount] = useState(0);
+
+  const handleBodyChange = (event) => {
     if (event.target.value.length <= 280) {
-      setText(event.target.value);
-      setCharacterCount(event.target.value.length);
+      setBodyText(event.target.value);
+      setBodyCharacterCount(event.target.value.length);
+    }
+  };
+
+  const handleTitleChange = (event) => {
+    if (event.target.value.length <= 30) {
+      setTitleText(event.target.value);
+      setTitleCharacterCount(event.target.value.length);
     }
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    setText("");
-    setCharacterCount(0);
+    setTitleText("");
+    setTitleCharacterCount(0);
+    setBodyText("");
+    setBodyCharacterCount(0);
   };
 
   return (
     <div>
-      <p className={`m-0 ${characterCount === 280 ? "text-error" : ""}`}>
-        Character Count: {characterCount}/280
-      </p>
       <form
         className="flex-row justify-center justify-space-between-md align-stretch"
         onSubmit={handleFormSubmit}
       >
         <textarea
-          placeholder="Here's a new thought..."
-          value={thoughtText}
+          placeholder="Whats the title of your review?"
+          value={titleText}
           className="form-input col-12 col-md-9"
-          onChange={handleChange}
+          onChange={handleTitleChange}
         ></textarea>
+        <p className={`m-0 ${titleCharacterCount === 30 ? "text-error" : ""}`}>
+          Character Count: {titleCharacterCount}/30
+        </p>
+        <textarea
+          placeholder="Here's a new review..."
+          value={bodyText}
+          className="form-input col-12 col-md-9"
+          onChange={handleBodyChange}
+        ></textarea>
+        <p className={`m-0 ${bodyCharacterCount === 280 ? "text-error" : ""}`}>
+          Character Count: {bodyCharacterCount}/280
+        </p>
         <button className="btn col-12 col-md-3" type="submit">
           Submit
         </button>
