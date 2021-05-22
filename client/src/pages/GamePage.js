04e1c26884f26f293af8e38679cdc25fc3ea5993
AlexDeLarge <jmrendon48@gmail.com>
2021-05-22 13:16:01 -0700

    linked search result game title to game page

---
+++ client/src/pages/GamePage.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Jumbotron, Container, Col, Form, Button, Card } from "react-bootstrap";
+
+const GamePage = () => {
+  return (
+    <>
+      <Container className="col-8">
+        <Card>
+          <p>GamePage</p>
+        </Card>
+      </Container>
+    </>
+  )
+};
+
+export default GamePage;
