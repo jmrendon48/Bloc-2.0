7ff6ad905d5b215975839958587daa022b2a40d2
AlexDeLarge <jmrendon48@gmail.com>
2021-05-22 19:19:13 -0700

    populated GamePage with data and modal

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useHistory, useLocation } from "react-router-dom";
-import { Jumbotron, Container, Col, Form, Button, Card } from "react-bootstrap";
+import { Jumbotron, Modal, Tab, Container, Col, Form, Button, Card } from "react-bootstrap";
+import ReviewForm from "../components/ReviewForm/index";
 
 const GamePage = (props) => {
   // const { name, coverId } = props.location.state
@@ -9,23 +11,39 @@ const GamePage = (props) => {
   const summary = props.location.state.summary;
   const first_release_date = props.location.state.first_release_date;
 
+  const [showReviewModal, setShowReviewModal] = useState(false);
+
   return (
     <>
       <Container className="col-8">
         <Card border="dark">
-            <Card.Img
-              src={coverId}
-              alt={`The cover for ${name}`}
-              variant="top"
-            />
           <Card.Title>
             {name} ({first_release_date})
           </Card.Title>
-          <Card.Body>
-            {summary}
-          </Card.Body>
+          <Card.Img src={coverId} alt={`The cover for ${name}`} variant="top" />
+          <Card.Body>{summary}</Card.Body>
+          <Card.Link onClick={() => setShowReviewModal(true)}>
+            <FontAwesomeIcon icon="plus-square" color="green" size="lg" /> Write a Bloc on this game!
+          </Card.Link>
         </Card>
       </Container>
+      {/* Add Review Modal */}
+      <Modal
+        size="lg"
+        show={showReviewModal}
+        onHide={() => setShowReviewModal(false)}
+        aria-labelledby="signup-modal"
+      >
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey="login">
+          <Modal.Header closeButton>
+            <Modal.Title id="signup-modal">Add Review</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <ReviewForm handleModalClose={() => setShowReviewModal(false)} />
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
     </>
   );
 };
